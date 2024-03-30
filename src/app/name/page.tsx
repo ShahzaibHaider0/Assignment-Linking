a079269a8866a7bab07fd94286d45e2ca991d995
ShahzaibHaider0 <150579950+ShahzaibHaider0@users.noreply.github.com>
2024-03-31 00:19:38 +0500

    Add files via upload

---
+++ src/app/name/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useRouter } from 'next/navigation';
+export default function GiveName() {
+  const router = useRouter();
+    return (
+      <div>
+            My name is Shahzaib Haider.
+      
+      </div>
+    )
+  }
